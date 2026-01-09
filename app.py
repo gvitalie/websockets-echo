0d0535da9d91a3626b650b7cc995ee237cd61b1d
Vitalie Ghelbert <gvitalie@gmail.com>
2026-01-09 21:49:52 +0200

    Updated websockets with multiple clients support.

--- app.py
+++ app.py
@@ -7,16 +7,27 @@ import signal
 
 from websockets.asyncio.server import serve
 
+# This will hold all active WebSocket connections
+clients = set()
 
 async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
-
+    # Register the new client
+    clients.add(websocket)
+    try:
+        async for message in websocket:
+            # Broadcast the message to all other connected clients
+            for client in clients:
+                if client != websocket:  # Don't send the message back to the sender
+                    await client.send(message)
+    except websockets.exceptions.ConnectionClosed:
+        pass
+    finally:
+        # Unregister the client when disconnected
+        clients.remove(websocket)
 
 def health_check(connection, request):
     if request.path == "/healthz":
         return connection.respond(http.HTTPStatus.OK, "OK\n")
-
 
 async def main():
     port = int(os.environ["PORT"])
@@ -24,7 +35,6 @@ async def main():
         loop = asyncio.get_running_loop()
         loop.add_signal_handler(signal.SIGTERM, server.close)
         await server.wait_closed()
-
 
 if __name__ == "__main__":
     asyncio.run(main())
